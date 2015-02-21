d504f6153b9ec9334c67f400db17e82123b8db47
Alexander Gaevsky <sasha@sasha0.ru>
2015-02-21 18:41:54 +0200

    added controller for tvseries details

--- episodes.py
+++ episodes.py
@@ -8,11 +8,15 @@ app.config.from_object('config.Config')
 api = Api(app)
 
 resource_fields = {
+    'id': fields.Integer,
     'title': fields.String,
     'rating': fields.String,
+    'short_description': fields.String,
     'description': fields.String,
     'genres': fields.List(fields.String),
-    'actors': fields.List(fields.String)
+    'actors': fields.List(fields.String),
+    'cover_thumbnail': fields.String,
+    'tvchannel': fields.String,
 }
 
 
@@ -37,8 +41,17 @@ class TVSeriesList(Resource):
         from models import TVSeries
         return TVSeries.query.paginate(page_id).items
 
-api.add_resource(TVSeriesList, '/series', '/series/<int:page_id>')
 
+class TVSeriesDetail(Resource):
+
+    @marshal_with(resource_fields)
+    def get(self, tvseries_id=1):
+        from models import TVSeries
+        return TVSeries.query.get(tvseries_id)
+
+
+api.add_resource(TVSeriesList, '/series', '/series/<int:page_id>')
+api.add_resource(TVSeriesDetail, '/series/i/<int:tvseries_id>')
 
 if __name__ == "__main__":
     app.run()
